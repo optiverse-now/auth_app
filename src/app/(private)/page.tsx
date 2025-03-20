9daa75a22c626b6672d9923011f8c7878bfa0d59
optiverse.now <optiverse.now@gmail.com>
2025-03-20 19:04:09 +0900

    uiの変更

---
+++ src/app/(private)/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div>
+      <div>ログイン後の画面</div>
+      <div>ログインアカウント</div>
+      <div>ログアウトボタン</div>
+    </div>
+  );
+}
---
+++ src/app/(public)/auth/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>AuthPage</h1>
+      <p>ログイン画面</p>
+    </div>
+  );
+}
