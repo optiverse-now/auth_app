fd80320917ce7b2265d1557f89456d28735470c9
optiverse-now <optiverse.now@gmail.com>
2025-03-22 20:53:31 +0900

    PrismaClientのインポートパスを修正

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@
 
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from ".prisma/client";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import { NextRequest } from "next/server";
